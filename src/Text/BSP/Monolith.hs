fa6523706ba25ce002649fafd5fb7ea915fe7f24
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-05-12 22:12:11 +0100

    BSP -> BSPT monad transformer.

--- src/Text/BSP/Monolith.hs
+++ src/Text/BSP/Monolith.hs
@@ -1,89 +1,103 @@
-{-# LANGUAGE DeriveFunctor, TupleSections #-}
+{-# LANGUAGE DeriveFunctor, TupleSections, ScopedTypeVariables, InstanceSigs #-}
 
 module Text.BSP.Monolith where
 
-import Control.Monad
 import Control.Applicative
+import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Except
 import Data.ByteString ( ByteString )
 import Data.ByteString.Internal ( c2w, w2c )
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C8
+import Data.Functor.Identity
 import Data.Word
 
 -- Simple result type for these parsers.
-type Result a = Either [String] (a, Int)
+-- Use runExceptT and runExcept for extraction of an Either.
+type MResult m a = ExceptT [String] m (a, Int)
+type Result a    = MResult Identity a
 
-newtype BSP a
-  = BSP { runBSP :: (ByteString, Int) -> Result a }
+newtype BSPT m a
+  = BSPT { runBSPT :: (ByteString, Int) -> MResult m a }
   deriving Functor
 
-instance Applicative BSP where
-  pure x = BSP (\ (_, off) -> Right (x, off))
+type BSP a = BSPT Identity a
 
-  pf <*> px = BSP $ \ (inp, off) -> do
-    (f, off')  <- runBSP pf (inp, off)
-    (x, off'') <- runBSP px (inp, off')
+runBSP :: BSP a -> (ByteString, Int) -> Result a
+runBSP p = runBSPT p
+
+instance Monad m => Applicative (BSPT m) where
+  pure x = BSPT (\ (_, off) -> return (x, off))
+
+  (<*>) :: BSPT m (a -> b) -> BSPT m a -> BSPT m b
+  pf <*> px = BSPT $ \ (inp, off) -> do
+    (f, off')  <- runBSPT pf (inp, off)
+    (x, off'') <- runBSPT px (inp, off')
     return (f x, off'')
 
-instance Monad BSP where
+instance Monad m => Monad (BSPT m) where
   return = pure
 
-  px >>= f = BSP $ \ (inp, off) -> do
-    (x, off') <- runBSP px (inp, off)
-    runBSP (f x) (inp, off')
+  px >>= f = BSPT $ \ (inp, off) -> do
+    (x, off') <- runBSPT px (inp, off)
+    runBSPT (f x) (inp, off')
 
-satisfy :: (Word8 -> Bool) -> BSP Word8
-satisfy p = BSP $ \ (inp, off) ->
+instance MonadTrans BSPT where
+  lift m = BSPT (\ (_, off) -> lift ((,off) <$> m))
+
+satisfy :: Monad m => (Word8 -> Bool) -> BSPT m Word8
+satisfy p = BSPT $ \ (inp, off) ->
   let chr = inp `BS.index` off
   in  if   p chr
-      then Right (chr, off + 1)
-      else Left $ [ concat [ "predicate does not match character "
-                           , show (fromC8 chr)
-                           , " in input "
-                           , show inp
-                           , " at offset "
-                           , show off
-                           ] ]
+      then return (chr, off + 1)
+      else throwE $ [ concat [ "predicate does not match character "
+                             , show (fromC8 chr)
+                             , " in input "
+                             , show inp
+                             , " at offset "
+                             , show off
+                             ] ]
 
-char :: Word8 -> BSP Word8
-char c = BSP $ \ (inp, off) ->
+char :: Monad m => Word8 -> BSPT m Word8
+char c = BSPT $ \ (inp, off) ->
   let chr = inp `BS.index` off
   in  if   c == chr
-      then Right (chr, off + 1)
-      else Left $ [ concat [ show (fromC8 c)
-                           , " does not match character "
-                           , show (fromC8 chr)
-                           , " in input "
-                           , show inp
-                           , " at offset "
-                           , show off
-                           ] ]
+      then return (chr, off + 1)
+      else throwE $ [ concat [ show (fromC8 c)
+                             , " does not match character "
+                             , show (fromC8 chr)
+                             , " in input "
+                             , show inp
+                             , " at offset "
+                             , show off
+                             ] ]
 
-string :: ByteString -> BSP ByteString
+string :: Monad m => ByteString -> BSPT m ByteString
 string str = foldr (\ c p -> char c *> p) (pure str) (BS.unpack str)
 
-chainr1 :: BSP a -> BSP (a -> a -> a) -> BSP a
+chainr1 :: Monad m => BSPT m a -> BSPT m (a -> a -> a) -> BSPT m a
 chainr1 px pop = loop
   where
     loop   = px >>= body
     body x = (($ x) <$> pop <*> loop) ||| pure x
 
-chainr :: BSP a -> BSP (a -> a -> a) -> a -> BSP a
+chainr :: Monad m => BSPT m a -> BSPT m (a -> a -> a) -> a -> BSPT m a
 chainr px pop x = chainr1 px pop ||| pure x
 
-chainl1 :: BSP a -> BSP (a -> a -> a) -> BSP a
+chainl1 :: Monad m => BSPT m a -> BSPT m (a -> a -> a) -> BSPT m a
 chainl1 px pop = px >>= loop
   where
     loop x = (($ x) <$> pop <*> px >>= loop) ||| pure x
 
-chainl :: BSP a -> BSP (a -> a -> a) -> a -> BSP a
+chainl :: Monad m => BSPT m a -> BSPT m (a -> a -> a) -> a -> BSPT m a
 chainl px pop x = chainl1 px pop ||| pure x
 
-peek :: BSP ByteString
-peek = BSP (\ (inp, off) -> Right (BS.drop off inp, off))
+peek :: Monad m => BSPT m ByteString
+peek = BSPT (\ (inp, off) -> return (BS.drop off inp, off))
 
-failure :: String -> BSP a
-failure msg = BSP (const (Left [msg]))
+failure :: Monad m => String -> BSPT m a
+failure msg = BSPT (const (throwE [msg]))
 
 coalesce :: (a -> a -> a) -> Either a b -> Either a b -> Either a b
 coalesce _ l@(Right _) _        = l
@@ -100,11 +114,11 @@ fromC8 :: Word8 -> Char
 fromC8 = w2c
 
 infixr 5 |||
-(|||) :: BSP a -> BSP a -> BSP a
-x ||| y = BSP $ \ (inp, off) ->
-  runBSP x (inp, off) `coalesceErrors` runBSP y (inp, off)
+(|||) :: Monad m => BSPT m a -> BSPT m a -> BSPT m a
+x ||| y = BSPT $ \ (inp, off) -> ExceptT $
+  coalesceErrors <$> runExceptT (runBSPT x (inp, off)) <*> runExceptT (runBSPT y (inp, off))
 
-instance Alternative BSP where
+instance Monad m => Alternative (BSPT m) where
   empty = failure "No such successful alternative found."
 
   (<|>) = (|||)
