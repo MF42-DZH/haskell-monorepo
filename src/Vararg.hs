210a46ad7c6330e3ac93d63188f2f8366f566bee
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-09-12 20:12:44 +0100

    Migrated ToColl and ArgList.

--- src/Vararg.hs
+++ src/Vararg.hs
@@ -1,7 +1,16 @@
-{-# LANGUAGE FlexibleInstances, FlexibleContexts, TypeOperators #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, TypeOperators, GADTs #-}
 {-# LANGUAGE UndecidableInstances, RankNTypes, FunctionalDependencies #-}
 
 module Vararg where
+
+import Data.Array ( Array )
+import qualified Data.Array as A
+import qualified Data.Foldable as F
+import Data.Sequence ( Seq(..), (|>) )
+import qualified Data.Sequence as SQ
+import Data.Set ( Set )
+import qualified Data.Set as St
+import Data.Void ( Void )
 
 class Constantly x r where
   constantly :: x -> r
@@ -23,3 +32,34 @@ instance {-# OVERLAPPABLE #-} ([r] ~ rs) => MZipI [r] rs where
 
 multizip :: MZipI [f] rs => f -> rs
 multizip f = collapse (repeat f)
+
+class ToColl a t | t -> a where
+  coll' :: Seq a -> a -> t
+
+instance ToColl a [a] where
+  coll' acc = F.toList . (acc |>)
+
+instance ToColl a (Seq a) where
+  coll' acc = (acc |>)
+
+instance Ord a => ToColl a (Set a) where
+  coll' acc x = F.foldl' (flip St.insert) St.empty (acc |> x)
+
+instance ToColl a (Array Int a) where
+  coll' acc x = A.listArray (0, SQ.length acc) (F.toList (acc |> x))
+
+instance ToColl a r => ToColl a (a -> r) where
+  coll' acc x = coll' (acc |> x)
+
+coll :: ToColl a r => a -> r
+coll = coll' SQ.empty
+
+infixr 5 :$:
+
+data ArgList f r t where
+  FN    :: ArgList r r Void
+  (:$:) :: b -> ArgList f r t -> ArgList (b -> f) r (ArgList f r t)
+
+apply :: f -> ArgList f r a -> r
+apply f FN        = f
+apply f (x :$: t) = apply (f x) t
