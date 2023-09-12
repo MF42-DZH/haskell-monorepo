432f207e109ce1e3b80ff69ca201e9c40c54cb03
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-09-12 19:56:41 +0100

    Working multizip, analogous to Clojure's map.

--- src/Vararg.hs
+++ src/Vararg.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, TypeOperators #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts, TypeOperators #-}
+{-# LANGUAGE UndecidableInstances, RankNTypes, FunctionalDependencies #-}
 
 module Vararg where
 
@@ -10,3 +11,15 @@ instance {-# OVERLAPPABLE #-} (x ~ x') => Constantly x x' where
 
 instance {-# OVERLAPPING #-} Constantly x b => Constantly x (a -> b) where
   constantly x _ = constantly x
+
+class MZipI fs rs | fs -> rs where
+  collapse :: fs -> rs
+
+instance {-# OVERLAPPING #-} MZipI [fs] rs => MZipI [a -> fs] ([a] -> rs) where
+  collapse fs as = collapse (zipWith ($) fs as)
+
+instance {-# OVERLAPPABLE #-} ([r] ~ rs) => MZipI [r] rs where
+  collapse = id
+
+multizip :: MZipI [f] rs => f -> rs
+multizip f = collapse (repeat f)
